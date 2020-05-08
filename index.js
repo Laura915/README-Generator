df77db7f4f773be27ca4d13b495b057333b4e70e
Laura <lhernan53112@gmail.com>
2020-05-08 17:13:03 -0400

    Added default to questions

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-//bring in all files needed
+//req files, module
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 const markDownFile = require("./utils/generateMarkdown");
-
-
-const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
     {
         type: "input",
         name: "username",
-        message: "Enter your GitHub username:"
+        message: "Enter your GitHub username:",
     },
     {
         type: "input",
@@ -22,22 +19,31 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Add a short description of your project:"
+        message: "Add a short description of your project:"  
     },
     {
-        type: "input",
+        type: "list",
         name: "license",
-        message: "What kind of licence does your project need:"
+        message: "What kind of licence does your project need:",
+        choices:[ 
+            "MIT", 
+            "APACHE2.0", 
+            "GPL3.0", 
+            "BSD3",
+            "None"
+        ]
     },
     {
         type: "input",
         name: "dependencies",
-        message: "What command should be run to insatll dependencies:"
+        message: "What command should be run to insatll dependencies:",
+        default: "npm i"
     },
     {
         type: "input",
         name: "test",
-        message: "What command should run to run test:"
+        message: "What command should be run to run test:",
+        default: "npm test"
     },
     {
         type: "input",
@@ -72,4 +78,3 @@ async function init() {
 }
 
 init();
-
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,50 @@
 function generateMarkdown(data) {
   return `
-  ![badge](https://img.shields.io/static/v1?label=licence&message=${data.license}&color=blue)
   # ${data.title}
 
-## Description 
-${data.description}
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#test)
-- [Questions](#questions)
+  ![badge](https://img.shields.io/static/v1?label=licence&message=${data.license}&color=blue)
 
-## Installation
-To install the required dependencies, run the following command:
-${data.dependencies}
-## Usage
-${data.usage}
-## License
-${data.license}
-## Contributing
-${data.contributing}
-`;
+ ## Description 
+ ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#test)
+
+  * [Questions](#questions)
+
+  ## Installation
+  To install required dependencies, run the following command:
+
+  \`\`\`
+  ${data.dependencies} 
+  \`\`\`
+
+  ## Usage
+  ${data.usage}
+  ## License
+  This project is licensed under the ${data.license} license
+  ## Contributing
+  ${data.contributing}
+  ## Test
+  To run test, run the following command:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+  If you have a question about this repo, open a issue or contact ${data.username} 
+
+  `;
 }
 
 module.exports = generateMarkdown;
